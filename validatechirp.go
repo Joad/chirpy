99fe6232fda271b16f1aaaac77b5030c7d084282
Joad <jesperbax@gmail.com>
2024-01-24 21:36:29 +0100

    Clean bad words in validation

--- validatechirp.go
+++ validatechirp.go
@@ -4,14 +4,15 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func validateChirp(w http.ResponseWriter, r *http.Request) {
 	type chirpBody struct {
 		Body string `json:"body"`
 	}
-	type chirpValid struct {
-		Valid bool `json:"valid"`
+	type chirpCleaned struct {
+		Cleaned string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -28,8 +29,26 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	validResp := chirpValid{
-		Valid: true,
+	badwords := map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+
+	validResp := chirpCleaned{
+		Cleaned: replaceBadWords(toValidate.Body, badwords),
 	}
 	respondWithJSON(w, 200, validResp)
+}
+
+func replaceBadWords(chirp string, badwords map[string]bool) string {
+	words := strings.Split(chirp, " ")
+	cleanedWords := make([]string, 0, len(words))
+	for _, word := range words {
+		if _, found := badwords[strings.ToLower(word)]; found {
+			word = "****"
+		}
+		cleanedWords = append(cleanedWords, word)
+	}
+	return strings.Join(cleanedWords, " ")
 }
